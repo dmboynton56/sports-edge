c313ac7631041cac9b0ba0d85449865982cfb4ae
Drew Boynton <drewboynton@Drews-MacBook-Air.local>
2026-08-13 09:11:19 -0600

    Align mobile grants with serving schema

--- data-core/sql/019_mobile_public_read_grants.sql
+++ data-core/sql/019_mobile_public_read_grants.sql
@@ -5,24 +5,17 @@
 grant select on games to anon, authenticated;
 grant select on odds_snapshots to anon, authenticated;
 grant select on model_predictions to anon, authenticated;
-grant select on games_today_enriched to anon, authenticated;
 
 grant select on model_evaluation_runs to anon, authenticated;
 grant select on strategy_backtest_results to anon, authenticated;
 grant select on model_evaluation_history to anon, authenticated;
 grant select on game_explanations to anon, authenticated;
-grant select on player_availability_reports to anon, authenticated;
-grant select on player_impact_estimates to anon, authenticated;
 
 grant select on pga_tournaments to anon, authenticated;
 grant select on pga_player_predictions to anon, authenticated;
 grant select on pga_odds_snapshots to anon, authenticated;
-grant select on pga_player_predictions_latest to anon, authenticated;
 grant select on mlb_home_run_predictions to anon, authenticated;
-grant select on mlb_home_run_predictions_latest to anon, authenticated;
-grant select on mlb_home_run_edges_latest to anon, authenticated;
 grant select on mlb_home_run_odds_snapshots to anon, authenticated;
-grant select on game_prediction_results to anon, authenticated;
 grant select on mlb_home_run_results to anon, authenticated;
 grant select on pga_prediction_results to anon, authenticated;
 
